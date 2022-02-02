884fd4b6a972a1e557ae7880e8d5275b0620554b
balejandra <alejandra.barreiro@alter-info.com>
2022-02-02 13:52:36 -0500

    titulo sistema

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,9 @@
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
         <span class="navbar-toggler-icon"></span>
     </button>
-
+    <div class="nav navbar-nav ml-auto">
+    <strong>SISTEMA DE CONTROL Y GESTIÓN DE ZARPES</strong>
+    </div>
     <ul class="nav navbar-nav ml-auto">
         <li class="nav-item d-md-down-none">
             <a class="nav-link" href="#">
