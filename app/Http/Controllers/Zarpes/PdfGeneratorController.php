9f134775b99f49bb2bd245698fb50c023e3ed1fb
Brenni Barreiro <alejandra.barreiro@alter-info.com>
2022-03-02 12:00:02 -0500

    PDF incompleto

--- app/Http/Controllers/Zarpes/PdfGeneratorController.php
+++ app/Http/Controllers/Zarpes/PdfGeneratorController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Zarpes;
 
 use App\Http\Controllers\Controller;
+use App\Models\Gmar\LicenciasTitulosGmar;
+use App\Models\Renave\Renave_data;
 use App\Models\Transaccion;
 use App\Models\Zarpes\PermisoZarpe;
+use App\Models\Zarpes\Tripulante;
 use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Http\Request;
 
@@ -12,8 +15,12 @@ class PdfGeneratorController extends Controller
 {
     public function imprimir($id){
         $trans= PermisoZarpe::all();
-        $tran= $trans->find($id);
-        $pdf=PDF::loadView('PDF.zarpes.permiso',compact('tran'));
-        return $pdf->stream('factura.pdf');
+        $zarpe= $trans->find($id);
+        $tran=Renave_data::where('matricula_actual',$zarpe->matricula)->first();
+        $tripulantes=Tripulante::select('ctrl_documento_id')->where('permiso_zarpe_id',$id)->where('capitan',true)->get();
+        $tripulantes=Tripulante::select('ctrl_documento_id')->where('permiso_zarpe_id',$id)->where('capitan',true)->get();
+        $trip= LicenciasTitulosGmar::whereIn('id',$tripulantes)->first();
+        $pdf=PDF::loadView('PDF.zarpes.permiso',compact('zarpe','tran','trip'));
+        return $pdf->stream('zarpes.pdf');
     }
 }
