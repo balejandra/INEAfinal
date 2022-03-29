b3d396b06797af5d5e355098e09eb3e51fc40d1c
Brenni Barreiro <alejandra.barreiro@alter-info.com>
2022-03-28 19:07:44 -0500

    Error Zarpes, formato correos

--- resources/views/emails/estadias/visita.blade.php
+++ resources/views/emails/estadias/visita.blade.php
@@ -2,7 +2,7 @@
 
     Saludos,
 
-    Su solicitud de Permiso de Estadia se le ha asignado un visitador, con el siguiente detalle:
+    Su Solicitud de Permiso de Estadia se le ha asignado un visitador, con el siguiente detalle:
 
 @component('mail::panel')
     <b>Nro de Solicitud:</b> {{$solicitud}} <br>
@@ -12,12 +12,12 @@
     <b>Fecha de Visita:</b>  {{$fecha_visita}} <br>
 
 @endcomponent
-    Para más detalles ingrese a la página web:
-@component('mail::button', ['url' => env('APP_URL')])
-    INEA
-@endcomponent
+@component('mail::subcopy')
     Instituto Nacional de Los Espacios Acuáticos - INEA. Síguenos en: <a href="http://twitter.com/#!/INEA200">http://twitter.com/#!/INEA200</a>
 
+@endcomponent
+@component('mail::footer')
     Sugerencia: Agregue {{$from}} a sus contactos de correo electrónico para así evitar recibir correo en spam.
     Gracias,
+@endcomponent
 @endcomponent
