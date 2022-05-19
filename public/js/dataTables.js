905161cdcccc3cac4907f0959cdc2ddffe69f71f
Brenni Barreiro <alejandra.barreiro@alter-info.com>
2022-05-18 20:21:28 -0500

    Datatable

--- public/js/dataTables.js
+++ public/js/dataTables.js
@@ -10,9 +10,8 @@ $(document).ready(function() {
             'copy', 'csv', 'excel', 'pdf', 'print'
         ]
     });
-} );
 
-$(document).ready(function() {
+
     $('button[data-toggle="tab"]').on( 'shown.bs.tab', function (e) {
         $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
     } );
@@ -33,10 +32,8 @@ $(document).ready(function() {
             'copy', 'csv', 'excel', 'pdf', 'print'
         ]
     });
-} );
 
 
-$(document).ready(function() {
     $('#table-nooptions').DataTable({
         responsive: true,
         language: {
@@ -46,9 +43,9 @@ $(document).ready(function() {
         "ordering": false,
         "info":     false
     });
-} );
 
-$(document).ready(function() {
+
+
     $('#table-nooptions-equipo').DataTable({
         "scrollX": true,
         language: {
@@ -57,9 +54,8 @@ $(document).ready(function() {
         "paging":   false,
         "info":     false
     });
-} );
 
-$(document).ready(function() {
+
     $('#table-scroll').DataTable({
         "scrollX": true,
         language: {
@@ -68,188 +64,5 @@ $(document).ready(function() {
         "paging":   false,
         "ordering": false,
         "info":     false
-    });
-} );
-
-
-
-$(document).ready(function() {
-    $('#TableRoles').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-
-    $('#permisoZarpes-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-
-    $('#TablePermissions').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        ordering: false,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-
-    });
-
-    $('#permisoZarpesdestino-table').DataTable({
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-$(document).ready(function() {
-    $('#TableMenus').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-$(document).ready(function() {
-    $('#dependenciaFederals-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-
-
-
-$(document).ready(function() {
-    $('#users-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-$(document).ready(function() {
-    $('#capitanias-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-$(document).ready(function() {
-    $('#auditables-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-
-
-
-$(document).ready(function() {
-    $('#menusroles-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-    $('#permisoEstadias-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-
-$(document).ready(function() {
-    $('#generic-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        dom: 'Blfrtp',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    });
-} );
-
-$(document).ready(function() {
-    $('#coordenadas-table').DataTable({
-        responsive: true,
-        fixedHeader: true,
-        language: {
-            "url": "../assets/DataTables/es_es.json"
-        },
-        "ordering": false,
-        "info":     false,
-        searching: false,
-         
     });
 } );
