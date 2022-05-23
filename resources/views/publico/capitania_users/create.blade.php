4d0835e158a844b3779bbdac7c0aa03819f301bd
Brenni Barreiro <alejandra.barreiro@alter-info.com>
2022-05-23 10:26:34 -0500

    validacione usuarios de capitanias

--- resources/views/publico/capitania_users/create.blade.php
+++ resources/views/publico/capitania_users/create.blade.php
@@ -19,6 +19,7 @@
      <div class="container-fluid">
           <div class="animated fadeIn">
               @include('flash::message')
+              @include('coreui-templates::common.errors')
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card">
