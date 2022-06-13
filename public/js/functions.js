103b8a49e43835d35954b4b31b39082253ef6f63
Carlos <carlos.arocha@alter-info.com>
2022-06-13 16:14:40 -0400

    modificación de paso cuatro fechas de calendario

--- public/js/functions.js
+++ public/js/functions.js
@@ -2141,3 +2141,9 @@ function validarExtension(idInput, divMsj) {
     }
   }
 
+
+  function noTeclas(event){
+    if((event.keyCode > 48 || event.keyCode < 57) && (event.keyCode > 96 || event.keyCode < 105) && event.keyCode !==190  && event.keyCode !==110 && event.keyCode !==8 && event.keyCode !==9  ){
+        return false;
+    }
+}
