e9f8f962cdb6b0d3fa5fc60086b26fe711f0b52f
Carlos <carlos.arocha@alter-info.com>
2022-06-13 16:20:38 -0400

    Modificacion de validaciones en carga de documentos de permiso de estadia

--- public/js/functions.js
+++ public/js/functions.js
@@ -2143,7 +2143,8 @@ function validarExtension(idInput, divMsj) {
 
 
   function noTeclas(event){
-    if((event.keyCode > 48 || event.keyCode < 57) && (event.keyCode > 96 || event.keyCode < 105) && event.keyCode !==190  && event.keyCode !==110 && event.keyCode !==8 && event.keyCode !==9  ){
+      console.log(event.keyCode);
+    if(event.keyCode!=37 && event.keyCode!=38 && event.keyCode!=39 && event.keyCode!=40  ){
         return false;
     }
 }
